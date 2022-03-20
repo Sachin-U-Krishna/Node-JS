8940052ea1d5afce47c508dd562ea6b96cd3e033
Sachin Krishna <liberty.sachin56@gmail.com>
2022-03-20 23:21:31 +0530

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -7,15 +7,14 @@ const connect = mongoose.connect(url)
 connect.then((db) => {
     console.log('Connected correctly to the server')
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
     })
-
-    newDish.save().then((dish) => {
+    .then((dish) => {
         console.log(dish._doc)
 
-        return Dishes.find({})
+        return Dishes.find({}).exec()
     })
     .then((dishes) => {
         console.log(dishes)
